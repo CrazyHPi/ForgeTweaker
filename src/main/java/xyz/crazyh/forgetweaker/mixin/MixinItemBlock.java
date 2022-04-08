7a22143fde0aa70454345d78ffddce60f7c92884
CrazyHPi <crazyhpi@outlook.com>
2022-04-08 18:09:21 +0800

    better placement ignore entity check

---
+++ src/main/java/xyz/crazyh/forgetweaker/mixin/MixinItemBlock.java
@@ -0,0 +1,12 @@
+package xyz.crazyh.forgetweaker.mixin;
+
+import net.minecraft.item.ItemBlock;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import xyz.crazyh.forgetweaker.config.Configs;
+
+@Mixin(ItemBlock.class)
+public class MixinItemBlock {
+
+}
--- src/main/java/xyz/crazyh/forgetweaker/mixin/MixinWorld.java
+++ src/main/java/xyz/crazyh/forgetweaker/mixin/MixinWorld.java
@@ -1,29 +1,42 @@
 package xyz.crazyh.forgetweaker.mixin;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xyz.crazyh.forgetweaker.config.Configs;
 
 @Mixin(World.class)
-public abstract class MixinWorld {
+public abstract class MixinWorld implements IBlockAccess, net.minecraftforge.common.capabilities.ICapabilityProvider{
+
     @Inject(
             method = "mayPlace",
             at = @At("HEAD"),
             cancellable = true
     )
     private void onMayPlace(Block blockIn, BlockPos pos, boolean skipCollisionCheck, EnumFacing sidePlacedOn, Entity placer, CallbackInfoReturnable<Boolean> cir) {
-        if (Configs.placeIgnoreEntity && placer instanceof EntityPlayer) {
-            if (((EntityPlayer) placer).isCreative()) {
+        //totally vanilla copy except entity collision check removed
+        if (Configs.placeIgnoreEntity && placer instanceof EntityPlayer && ((EntityPlayer) placer).isCreative()) {
+            //forge event stuff copy
+            if (!((placer instanceof EntityPlayer) || !net.minecraftforge.event.ForgeEventFactory.onBlockPlace(placer, new net.minecraftforge.common.util.BlockSnapshot(placer.world, pos, blockIn.getDefaultState()), sidePlacedOn).isCanceled())) {
+                cir.setReturnValue(false);
+            }
+            //vanilla stuff copy
+            IBlockState iblockstate1 = this.getBlockState(pos);
+            if (iblockstate1.getMaterial() == Material.CIRCUITS && blockIn == Blocks.ANVIL) {
                 cir.setReturnValue(true);
+            } else {
+                cir.setReturnValue(iblockstate1.getBlock().isReplaceable(this, pos) && blockIn.canPlaceBlockOnSide(placer.world, pos, sidePlacedOn));
             }
         }
     }
